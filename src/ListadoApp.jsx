79eb9fb93abfd3b62ceb87619062fc68f76a73a1
leenh linux <hackingleenh@gmail.com>
2023-10-25 18:01:58 -0500

    mapeo de lisats

--- src/ListadoApp.jsx
+++ src/ListadoApp.jsx
@@ -1,8 +1,9 @@
+import React, { useState } from "react";
 import { FaCheck, FaMinusCircle } from "react-icons/fa";
 
-const Items = ({ nombre, visto }) => {
+const Items = ({ nombre, visto, id }) => {
   return (
-    <li>
+    <li data-id={id}>
       {nombre}
       {/* {visto ? (
         <span>
@@ -11,25 +12,46 @@ const Items = ({ nombre, visto }) => {
       ) : (
         <FaMinusCircle className="icon danger"></FaMinusCircle>
       )} */
-      /* en lugar de suar ?? se usa && */}
+      /* en lugar de usar ?? se usa && */}
       {visto && <FaCheck className="icon success" />}
     </li>
   );
 };
 
 export const ListadoApp = () => {
+  let listadoSecciones = [
+    { nombre: "Instalación", visto: true },
+    { nombre: "uso de vite", visto: true },
+    { nombre: "Componentes", visto: true },
+    { nombre: "Variables", visto: true },
+    { nombre: "useState", visto: true },
+    { nombre: "Redux", visto: false },
+    { nombre: "custonHoko", visto: false },
+  ];
+
+  const [arreglo, setArreglo] = useState(listadoSecciones);
+
+  const agregarNuevaTarea = () => {
+    let tarea = document.getElementById("tarea").value;
+    let visto = document.getElementById("visto").checked;
+    console.log({ nombre: tarea, visto: visto });
+    setArreglo([...arreglo, { nombre: tarea, visto: visto }]);
+  };
+
   return (
     <>
       <h1>Listado de Temas del Curso</h1>
       <ol>
-        <Items nombre="Instalación" visto={true}></Items>
-        <Items nombre="uso de vite" visto={true}></Items>
-        <Items nombre="Componentes" visto={true}></Items>
-        <Items nombre="Variables" visto={true}></Items>
-        <Items nombre="useState" visto={true}></Items>
-        <Items nombre="Redux" visto={false}></Items>
-        <Items nombre="custonHoko" visto={false}></Items>
+        {arreglo.map((row, index) => (
+          // version corta
+          <Items id={index} key={index} {...row} />
+          //   version larga
+          //   <Items id={index} key={index} nombre={row.nombre} visto={row.visto} />
+        ))}
       </ol>
+      <input type="text" id="tarea" />
+      <input type="checkbox" id="visto" />
+      <button onClick={() => agregarNuevaTarea()}>Agregar Nuevo</button>
     </>
   );
 };
